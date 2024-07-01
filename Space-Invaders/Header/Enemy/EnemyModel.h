d45fa43feb03de70462d3cb7f500b4aefb819eb3
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-07-01 07:03:21 +0530

    Commit

--- Space-Invaders/Header/Enemy/EnemyModel.h
+++ Space-Invaders/Header/Enemy/EnemyModel.h
@@ -12,7 +12,7 @@ namespace Enemy
     class EnemyModel
     {
     private:
-        sf::Vector2f reference_position = sf::Vector2f(50.f, 50.f);
+        sf::Vector2f reference_position = sf::Vector2f(50.f, 100.f);
         sf::Vector2f enemy_position;
 
         MovementDirection movement_direction;
@@ -22,8 +22,8 @@ namespace Enemy
 
     public:
 
-        const sf::Vector2f left_most_position = sf::Vector2f(50.f, 950.f);
-        const sf::Vector2f right_most_position = sf::Vector2f(1800.f, 950.f);
+        const sf::Vector2f left_most_position = sf::Vector2f(50.f, 100.f);
+        const sf::Vector2f right_most_position = sf::Vector2f(1800.f, 100.f);
 
         const float vertical_travel_distance = 100.f;
         const float enemy_movement_speed = 250.0f;
