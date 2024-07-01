fd20fab9204963efbc6559954f0b1c665cb81707
DESKTOP-9K53R7V\MSI <abhaytiwariwork0@gmail.com>
2024-07-01 14:21:36 +0530

    BulletController

--- Space-Invaders/Header/Bullet/BulletConfig.h
+++ Space-Invaders/Header/Bullet/BulletConfig.h
@@ -1,6 +1,17 @@
 #pragma once
-namespace Bullet {
 
+namespace Bullet
+{
+	enum class BulletType
+	{
+		LASER_BULLET,
+		TORPEDO,
+		FROST_BULLET,
+	};
 
-	
+	enum class MovementDirection
+	{
+		UP,
+		DOWN,
+	};
 }
